f23c23896b3a0cbe925330b8b8d00715bb7c0bda
Angshuman <angshumankashyap69yap69@gmail.com>
2025-06-09 21:01:20 +0530

    3rd commit done

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 let dragCard=null;
+let showcontext=null;
 function AddTask(tasks){
     const task=document.getElementById(`${tasks}-input`);
-    if(task.value==''){
+    if(task.value.trim()==''){
         return;
     }
-    const card=CreateCard(task.value);
+    const datetask=new Date().toLocaleString();
+    // console.log(datetask);
+    const card=CreateCard(task.value, datetask);
     document.getElementById(`${tasks}-task`).appendChild(card);
     task.value="";
+    updatecount(tasks);
 }
 
-function CreateCard(tsk){
+function CreateCard(tsk, datetask){
     const task=document.createElement("div");
-    task.textContent=tsk;
+        task.innerHTML=`
+        <span>${tsk}</span><br>
+        <small class="time">${datetask}</small>
+        `
     task.classList.add("card");
     task.draggable=true;
     task.addEventListener("dragstart",dragStart);
     task.addEventListener("dragend",dragEnd);
+    task.addEventListener("contextmenu",function (ele){
+        ele.preventDefault();
+        showcontext=this;
+        addcontextMenu(ele.pageX, ele.pageY);
+    });
     return task;
 }
 
@@ -26,6 +38,9 @@ function dragStart(){
 function dragEnd(){
     this.classList.remove("dragging");
     dragCard=null;
+    ["task","doing","done"].forEach((event)=>{
+        updatecount(event);
+    })
 }
 
 const columns=document.querySelectorAll(".column .tasks");
@@ -34,7 +49,49 @@ columns.forEach((column) => {
     column.addEventListener("dragover",dragOver);})
 
 function dragOver(elem){
-    console.log(elem);
+    // console.log(elem);
     elem.preventDefault();
     this.appendChild(dragCard);
 }    
+
+let contextMenu=document.querySelector('.context-menu');
+function addcontextMenu(x, y){
+    // console.log(x,y);
+    contextMenu.style.left=`${x}px`;
+    contextMenu.style.top=`${y}px`;
+    contextMenu.style.display='block';
+}
+
+
+document.addEventListener("click",()=>{
+    // console.log("click");
+    contextMenu.style.display='none';
+})
+
+
+function editTask(){
+    if(showcontext!==null){
+        let newtext= prompt("Edit task:",showcontext.querySelector("span").textContent);
+        newtext=newtext.trim();
+        const datetask=new Date().toLocaleString();
+        if(newtext!==""){
+            showcontext.innerHTML=`
+        <span>${newtext}</span><br>
+        <small class="time">${datetask}</small>
+        `
+        }
+    }
+}
+
+function deleteTask(){
+    if(showcontext!==null){
+        let columnId=showcontext.parentElement.parentElement.id;
+        showcontext.remove();
+        updatecount(columnId);
+    }
+}
+
+function updatecount(columnId){
+    let count= document.querySelectorAll(`#${columnId}-task .card`).length;
+    document.querySelector(`#${columnId}-count`).textContent=count;
+}
